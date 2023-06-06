5d979c8476dd67259baf57eafa341ff243d68407
Alejandra Paez <alepaezdev@gmail.com>
2023-06-05 21:50:13 -0600

    feat: list koders ep

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
+const fsPromises = require("fs/promises");
 
 // Endpoint de Home
 app.get("/", (req, res) => {
@@ -7,7 +9,23 @@ app.get("/", (req, res) => {
 })
 
 // Endpoints
+/**
+ * 1er endpoint
+ * Enlistar koders.
+ * ARQ REST
+ * recurso/identificar 
+ * method -> GET
+ * ruta -> /koders
+ * 
+ */
 
+app.get("/koders", async (req, res) => {
+  const db = await fsPromises.readFile("./koders.json", "utf8");
+  const parsedDB = JSON.parse(db);
+  console.log("DB no parseada", db);
+  console.log("DB si parseada", parsedDB);
+  res.json(parsedDB);
+})
 
 app.listen(8080, () => {
   console.log("Nuestro servidor esta prendido");
