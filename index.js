62b4fef67090818c362cecf921cdaf70660af334
Alejandra Paez <alepaezdev@gmail.com>
2023-06-06 19:35:01 -0600

    feat: ejercicio

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const fsPromises = require("fs/promises");
+const { join } = require("path");
 
 // Endpoint de Home
 app.get("/", (req, res) => {
@@ -18,19 +19,32 @@ app.get("/", (req, res) => {
  *
  */
 
+/**
+ * En el lado de backend
+ * Path params -> modifican la ruta agregando :pathParam
+ * Query params -> no modifican la ruta
+ */
 // List koders
 app.get("/koders", async (req, res) => {
+    const { module } = req.query;
+    console.log("req.query", req.query)
     const db = await fsPromises.readFile("./koders.json", "utf8"); // leemos base de datos
     const parsedDB = JSON.parse(db); // parseamos json
-    console.log("DB no parseada", db);
-    console.log("DB si parseada", parsedDB);
-    res.json(parsedDB); // respondemos con header de Content-Type -> application/json
+    const filteredKoders = parsedDB.koders.filter(koder => module === koder.module)
+    // Esta vacio, no filtro
+    if(filteredKoders.length === 0) {
+        res.json(parsedDB.koders)
+    } else {
+        res.json(filteredKoders); // respondemos con header de Content-Type -> application/json
+    }
 });
 
 // Get Koder by name
 /**
  * Query params ->>>> ?modulo=Backend
  * Path params(identificador) ->>>> :name
+ * 
+ * ?postId=2
  */
 app.get("/koders/:name", async (req, res) => {
     // Path param
@@ -46,3 +60,11 @@ app.get("/koders/:name", async (req, res) => {
 app.listen(8080, () => {
     console.log("Nuestro servidor esta prendido");
 });
+
+
+/**
+ * Ejercicio
+ * 2 endpoints -
+ * 1.er Donde me filtren por age los mentores
+ * 2.do Donde obtengamos un mentor en especifico con su nombre
+ */
